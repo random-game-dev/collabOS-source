506745110d6ec8f0236798c85b7d6470504b41e2
alkonsafe <philblakeschool@outlook.com>
2025-08-17 20:09:49 -0400

    Add files via upload

---
+++ CosmosKernel8/Kernel.cs
@@ -0,0 +1,77 @@
+﻿using Cosmos.System.Graphics;
+using System;
+using System.Drawing;
+using Sys = Cosmos.System;
+using Cosmos.System.Graphics.Fonts;
+
+namespace GraphicTest
+{
+    public class Kernel : Sys.Kernel
+    {
+        Canvas canvas;
+
+        private readonly Bitmap bitmap = new Bitmap(10, 10,
+                new byte[] { 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0,
+                    255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255,
+                    0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255,
+                    0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 23, 59, 88, 255,
+                    23, 59, 88, 255, 0, 255, 243, 255, 0, 255, 243, 255, 23, 59, 88, 255, 23, 59, 88, 255, 0, 255, 243, 255, 0,
+                    255, 243, 255, 0, 255, 243, 255, 23, 59, 88, 255, 153, 57, 12, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255,
+                    243, 255, 0, 255, 243, 255, 153, 57, 12, 255, 23, 59, 88, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243,
+                    255, 0, 255, 243, 255, 0, 255, 243, 255, 72, 72, 72, 255, 72, 72, 72, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0,
+                    255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 72, 72,
+                    72, 255, 72, 72, 72, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255,
+                    10, 66, 148, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255,
+                    243, 255, 10, 66, 148, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 10, 66, 148, 255, 10, 66, 148, 255,
+                    10, 66, 148, 255, 10, 66, 148, 255, 10, 66, 148, 255, 10, 66, 148, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255,
+                    243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 10, 66, 148, 255, 10, 66, 148, 255, 10, 66, 148, 255, 10, 66, 148,
+                    255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255,
+                    0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, }, ColorDepth.ColorDepth32);
+
+        protected override void BeforeRun()
+        {
+            // If all works correctly you should not really see this :-)
+            Console.WriteLine("Cosmos booted successfully. Let's go in Graphical Mode");
+
+            /*
+            You don't have to specify the Mode, but here we do to show that you can.
+            To not specify the Mode and pick the best one, use:
+            canvas = FullScreenCanvas.GetFullScreenCanvas();
+            */
+            canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(640, 480, ColorDepth.ColorDepth32));
+
+            // This will clear the canvas with the specified color.
+            canvas.Clear(Color.Black);
+        }
+
+        protected override void Run()
+        {
+            try
+            {
+                // A red Point
+                canvas.DrawPoint(Color.Red, 69, 69);
+
+                // Top horizontal line (moved right 5, up 10)
+                canvas.DrawLine(Color.GreenYellow, 210, 155, 400, 155);
+
+                // Left vertical line (moved right 5, up 10)
+                canvas.DrawLine(Color.IndianRed, 210, 155, 210, 305);
+
+                // Bottom horizontal line (moved right 5, up 10)
+                canvas.DrawLine(Color.MintCream, 210, 305, 400, 305);
+
+                Font font = PCScreenFont.Default;
+                canvas.DrawString("Welcome to CollabOS", font, Color.White, 230, 380);
+                
+                canvas.Display(); // Required for something to be displayed when using a double buffered driver
+
+                Console.ReadKey();
+                Sys.Power.Shutdown();
+            }
+            catch (Exception e)
+            {
+                Sys.Power.Shutdown();
+            }
+        }
+    }
+}
